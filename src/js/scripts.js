23a4bc4bba35c470f54f04d1a9a45bd161958995
Dziuba Alexei <dziuba88@gmail.com>
2019-10-03 12:47:41 +0300

    vacancies

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,8 @@
 //@prepros-append ./plugins/jquery-1.12.4.min.js
 //@prepros-append ./plugins/dropdown.js
-//@prepros-append ./plugins/tab.js
+//@prepros-append ./plugins/tabs.js
+//@prepros-append ./plugins/collapse.js
 //@prepros-append ./plugins/transition.js
 //@prepros-append ./plugins/selectric.js
 //@prepros-append ./plugins/carousel.js
+//@prepros-append ./plugins/upload.js
